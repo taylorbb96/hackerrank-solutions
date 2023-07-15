64c0805f9eded9240b9c048b0ab6485a8b16a8c4
Taylor Brown <taylorbb96@gmail.com>
2023-07-15 03:22:47 -0500

    Add 30 days of code day 1

---
+++ 30-days-of-code/data-types.py
@@ -0,0 +1,17 @@
+i = 4
+d = 4.0
+s = 'HackerRank '
+my_int = 0
+my_double = 0.0
+my_string = ''
+
+my_int = int(input())
+my_double = float(input())
+my_string += input()
+
+print(i+my_int)
+
+
+print(d+my_double)
+
+print(s+my_string)
